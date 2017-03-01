8c086113f3ae2d903eecd73487515782225f7285
cloudburst <langmuir.cloudburst@gmail.com>
2017-03-01 14:54:15 -0800

    tweak setup.py spacing

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@
 
 from distutils.core import setup
 
-setup(name='libheap',
-      version='0.1',
-      description='gdb python library for examining the glibc heap (ptmalloc)',
-      author='cloud',
-      url='https://github.com/cloudburst/libheap',
-      license="MIT",
-      keywords="ptmalloc gdb python glibc",
-      py_modules=['libheap', 'printutils', 'prettyprinters']
-     )
+setup(
+    name        = 'libheap',
+    packages    = ['libheap'],
+    version     = '0.1',
+    description = 'gdb python library for examining the glibc heap (ptmalloc)',
+    author      = 'cloud',
+    url         = 'https://github.com/cloudburst/libheap',
+    license     = 'MIT',
+    keywords    = 'ptmalloc gdb python glibc',
+    py_modules  = ['libheap', 'printutils', 'prettyprinters']
+)
